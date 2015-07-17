96abc1dc6c9042a6ae6a3346e9493981a91ab359
Shivaji Dutta <dutta_shivaji@yahoo.co.uk>
2015-07-17 15:52:29 -0700

    reorganized the directory structure

--- Sqoop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# A Simple Shell Script To get Sqoop running
-# Author - Shivaji Dutta
-# Change the ip address to the ipadderss of the mysql server
-
-sqoop import --connect jdbc:mysql://172.16.106.207:3306/test --username trucker --password trucker --table DRIVERS -m 1 --target-dir demo1 --hive-import --hive-table drivers
--- flume.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-flume-ng agent -c /etc/flume/conf -f /etc/flume/conf/flume.conf -n sandbox
